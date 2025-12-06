a6c314f1a2b79a3aee98b123e7826310298fffc3
Andrii Moshurenko <moshurenkoaa@gmail.com>
2025-12-06 22:37:42 +0100

    Export all tools from tools module
    Added missing exports:
    - analyze_website, monitor_trends (web)
    - generate_code, review_code (code)
    - delete_path, copy_path, move_path (file)

--- agent_hive/tools/__init__.py
+++ agent_hive/tools/__init__.py
@@ -1,15 +1,42 @@
 """Tools for Agent Hive drones."""
 
-from agent_hive.tools.web_tools import web_search, fetch_url
-from agent_hive.tools.code_tools import execute_python, write_file, read_file
-from agent_hive.tools.file_tools import list_directory, create_directory
+from agent_hive.tools.web_tools import (
+    web_search,
+    fetch_url,
+    analyze_website,
+    monitor_trends,
+)
+from agent_hive.tools.code_tools import (
+    execute_python,
+    write_file,
+    read_file,
+    generate_code,
+    review_code,
+)
+from agent_hive.tools.file_tools import (
+    list_directory,
+    create_directory,
+    delete_path,
+    copy_path,
+    move_path,
+)
 
 __all__ = [
+    # Web tools
     "web_search",
     "fetch_url",
+    "analyze_website",
+    "monitor_trends",
+    # Code tools
     "execute_python",
     "write_file",
     "read_file",
+    "generate_code",
+    "review_code",
+    # File tools
     "list_directory",
     "create_directory",
+    "delete_path",
+    "copy_path",
+    "move_path",
 ]
